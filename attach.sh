f8e5000fb84f898da876d790428ddeca905ee063
Kim, Joo-Ho <teamsprint@gmail.com>
2019-10-18 03:37:57 +0900

    add sudo

--- attach.sh
+++ attach.sh
@@ -2,5 +2,5 @@
 
 CONTAINER_NAME=metatron
 
-docker exec -it $CONTAINER_NAME /bin/bash
+sudo docker exec -it $CONTAINER_NAME /bin/bash
 
--- destroy.sh
+++ destroy.sh
@@ -2,5 +2,5 @@
 
 CONTAINER_NAME=metatron
 
-docker rm -f $CONTAINER_NAME
+sudo docker rm -f $CONTAINER_NAME
 
